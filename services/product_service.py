299123f5d760722f071b07bad0c5479b576ad7de
Talya2004 <147398638+Talya2004@users.noreply.github.com>
2024-07-29 16:12:21 +0300

    Update product_service.py
    validation on price_subscription and price_monthly 
    in update and post

--- services/product_service.py
+++ services/product_service.py
@@ -19,8 +19,15 @@ class product_service(base_service):
             return self.repo.delete_by_category(category)
 
         def create(self, data):
-            print(f'product service create {data}')
+            # ולידציה לשדות המחיר
+            if not isinstance(data.get('price_subscription'), (int, float)) or not isinstance(data.get('price_monthly'), (int, float)):
+                abort(400, 'price_subscription ו-price_monthly חייבים להיות מספרים תקינים.')
+
             return self.repo.insert(data)
 
         def update(self, category, data):
-            return self.repo.update_by_category(category , data)
+            # ולידציה לשדות המחיר
+            if not isinstance(data.get('price_subscription'), (int, float)) or not isinstance(data.get('price_monthly'),(int, float)):
+                abort(400, 'price_subscription ו-price_monthly חייבים להיות מספרים תקינים.')
+
+            return self.repo.update_by_category(category, data)
