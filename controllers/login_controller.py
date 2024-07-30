6528d67fc6e93c5ff7a199964b0db968e97984d0
SaraHaouzi <147398577+SaraHaouzi@users.noreply.github.com>
2024-07-30 12:22:29 +0300

    Update login_controller.py cors1

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -184,15 +184,23 @@ class PasswordResetResponse(Resource):
 class Logout(Resource):
     @jwt_required()
     def options(self):
-        """
-        מתודת OPTIONS - מאפשרת בקשות CORS.
-        """
-        return {'Allow': 'POST, OPTIONS'}, 200, {
-            'Access-Control-Allow-Origin': "https://kostiner-tenders.onrender.com",
-            'Access-Control-Allow-Methods': 'POST, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Allow-Credentials': 'true'
-        }
+    origin = request.headers.get('Origin')
+        allowed_origins = ["https://kostiner-tenders.onrender.com", "http://localhost:5174","http://localhost:5173"]
+
+        if origin in allowed_origins:
+            return {'Allow': 'POST, OPTIONS'}, 200, {
+                'Access-Control-Allow-Origin': origin,
+                'Access-Control-Allow-Methods': 'POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true'
+            }
+        else:
+            return {'Allow': 'POST, OPTIONS'}, 403, {
+                'Access-Control-Allow-Origin': 'null',
+                'Access-Control-Allow-Methods': 'POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Credentials': 'true'
+            }
 
     @jwt_required()
     def post(self):
