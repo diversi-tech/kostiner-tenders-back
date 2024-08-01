7bf24f2ba8987a55d6b79ed1a7e4aeb337a375ab
Talya2004 <147398638+Talya2004@users.noreply.github.com>
2024-08-02 01:40:20 +0300

    Update product_controller.py

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -12,7 +12,6 @@ from services.auth_service import policy_required
 class GetAllProducts(Resource):
     @namespace.doc('list_products')
     @namespace.marshal_list_with(product_model)
-    @jwt_required()
     def get(self):
         '''get all product'''
         return product_service.get_all()
