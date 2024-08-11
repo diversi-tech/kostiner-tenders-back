081cbe54cd2e616f2ab3071e4823e52b671ff1fd
SaraHaouzi <147398577+SaraHaouzi@users.noreply.github.com>
2024-08-11 11:27:40 +0300

    Update app.py token

--- app/app.py
+++ app/app.py
@@ -25,7 +25,7 @@ authorizations = {
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'chgc#sd1'  # Change to your actual secret key
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=3)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=3650)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=3650)
 
 app.config.update(
